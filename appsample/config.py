a21ca085dd4dd41f4e61bdb08f22c7a87e75c6ba
哲維 劉 <q5040842@gmail.com>
2022-06-22 17:20:20 +0800

    feature: add homepage func

--- appsample/config.py
+++ appsample/config.py
@@ -33,9 +33,9 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = False
     WTF_CSRF_CHECK_DEFAULT = True
     WTF_CSRF_SSL_STRICT = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/manga"
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/manga"
     # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost:5432/manga"
-    # SQLALCHEMY_DATABASE_URI = "postgresql://povelhruklfysu:f22f3cfc2155dba0953fa4d83eebff633c5406719814a0655d952030fe758d0c@ec2-18-214-134-226.compute-1.amazonaws.com:5432/dfmps0s1nca2kv"
+    SQLALCHEMY_DATABASE_URI = "postgresql://povelhruklfysu:f22f3cfc2155dba0953fa4d83eebff633c5406719814a0655d952030fe758d0c@ec2-18-214-134-226.compute-1.amazonaws.com:5432/dfmps0s1nca2kv"
     # if use docker compose use this
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@mysql:3306/manga"
 
