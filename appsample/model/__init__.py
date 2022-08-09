31ac3f0c13dd579b4d15a978f16d246b06d3b38d
哲維 劉 <q5040842@gmail.com>
2022-08-09 16:09:51 +0800

    feature: 新增個人的上傳紀錄相關UI及功能

--- appsample/model/__init__.py
+++ appsample/model/__init__.py
@@ -1,1 +1,1 @@
-from .model import db, select, sqlOP, migrate, User, Permission, Role, Manga, AnonymousUser, Likes
+from .model import db, select, sqlOP, migrate, User, Permission, Role, Manga, AnonymousUser, Likes, get_random
